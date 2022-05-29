a9ec8c7af1c7d9dd944a0914f22cd82d01f6ae48
hbtidotgkb <gkb2004bhardwaj@gmail.com>
2022-05-29 17:59:10 +0530

    modified problem desc

--- react-projects/edtech-react-app/src/component/algorithms/SingleProblem.js
+++ react-projects/edtech-react-app/src/component/algorithms/SingleProblem.js
@@ -1,9 +1,29 @@
 function SingleProblem(props) {
+    const spanVideoLinkStyle = {
+        textDecorations:'none',
+        color:'red'
+    }
+    const spanNormalStyle = {
+        textDecorations:'none',
+        fontWeight:'normal'
+    }
     const element =
     <div>
-        <h4>Problem Title: {props.name}</h4>
-        <h4>Problem Description: </h4><p>{props.desc}</p>
-        <a href={"https://d26glhxwb4kju2.cloudfront.net/" +props.name+".mp4"}>Two Sum Video Solution</a>
+        <div>
+        <div>
+            <h4>Problem Title: 
+                <span style={spanNormalStyle}>
+                {"  "+props.name} [LC# 1] 
+                   (<a href={"https://d26glhxwb4kju2.cloudfront.net/" +props.name+".mp4"} style={spanVideoLinkStyle}>Video Solution</a>)
+                </span>
+            </h4>
+        </div>
+        </div>
+        <h4>Problem Description: 
+            <span style={spanNormalStyle}>
+            {"  "+props.desc}
+            </span>
+        </h4>
     </div>
     return element;
 }
