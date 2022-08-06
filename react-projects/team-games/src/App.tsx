6b298cce77e94a38ca0c250663a3d1e557b4ff7e
hbtidotgkb <gkb2004bhardwaj@gmail.com>
2022-08-06 18:54:47 +0530

    bdrs

---
+++ react-projects/team-games/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import bdrs_bg from './images/bdrs_bg.png';
+
+function App() {
+  return (
+    <div>
+      {/* <header className="App-header"> */}
+        <img src={bdrs_bg} className="Bdrs-Bg" alt="logo" />
+      {/* </header> */}
+    </div>
+  );
+}
+
+export default App;
